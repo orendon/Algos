66858a2c859f14d419f8e7332eb7457ef61cae5f
Oscar Rendon <orendon@gmail.com>
2014-02-16 11:50:34 -0500

    Validates row and column indexes.

--- src/Percolation/Percolation.java
+++ src/Percolation/Percolation.java
@@ -1,11 +1,48 @@
 /*
- * Implements percolation using quickfind algorithm
+ * Percolation system using WeightedQuickUnionUF algorithm
+ * Monte Carlo simulation will be used to calculate threshold value
  *
  * @author Oscar Rendon
  */
 
 public class Percolation {
+    
+    private int n;
+    
+    // Create a NxN grid with all sites closed
     public Percolation(int N) {
-        if (N <= 0) throw new IndexOutOfBoundsException("Invalid N-by-N Grid.");
+        if (N <= 0) throw new IllegalArgumentException("Invalid grid size.");
+        
+        n = N;
+    }
+    
+    // Open the site on row i and column j
+    public void open(int i, int j) {
+        validateRowRanges(i, j);
+    }
+    
+    // Check if a given site is open
+    public boolean isOpen(int i, int j){
+        validateRowRanges(i, j);
+        
+        return false;
+    }
+    
+    // Check if a given  site is fully open
+    public boolean isFull(int i, int j) {
+        validateRowRanges(i, j);
+        
+        return false;
+    }
+    
+    // Indicates if the system percolates
+    public boolean percolates() {
+        return false;
+    }
+    
+    private void validateRowRanges(int i, int j) {
+        if (i <= 0 || i > n || j <= 0 || j > n) {
+            throw new IndexOutOfBoundsException("Invalid row, column indices.");
+        }
     }
 }
--- test/PercolationTest.java
+++ test/PercolationTest.java
@@ -1,19 +1,40 @@
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 public class PercolationTest {
     
-    public PercolationTest() {
+    final int N = 10;
+    private Percolation percolation;
+    
+    @Before
+    public void setUp() {
+        percolation = new Percolation(N);
     }
     
-    @Test(expected=IndexOutOfBoundsException.class)
+    @Test(expected=IllegalArgumentException.class)
     public void testZeroGridArguments() {
         Percolation percolation = new Percolation(0);
     }
     
-    @Test(expected=IndexOutOfBoundsException.class)
+    @Test(expected=IllegalArgumentException.class)
     public void testNegativeGridArguments() {
         Percolation percolation = new Percolation(-1);
+    }
+    
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testInvalidRangeWhileOpeningASite() {
+        percolation.open(0, 0);
+    }
+    
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testInvalidRangeWhileCheckingOpenSite() {
+        percolation.isOpen(N, N+1);
+    }
+    
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testInvalidRangeWhileCheckingFullSite() {
+        percolation.isFull(N+1, 0);
     }
     
 }
